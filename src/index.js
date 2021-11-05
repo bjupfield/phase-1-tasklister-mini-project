1a14c8ab248ac1ad40f1c83d28ff82355909c7cb
Bennett <bjupfield@gmail.com>
2021-11-05 14:06:58 -0500

    couldnt get select to work :(

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener("submit", addTask)
 });
+function addTask(event){
+  event.preventDefault()
+  if(document.getElementById("new-task-description").value != ""){
+    const li =  document.createElement("li");
+    li.textContent = document.getElementById("new-task-description").value
+    const btn = document.createElement("button")
+    btn.textContent = "x"
+    btn.addEventListener("click", delet)
+    li.appendChild(btn);
+    document.querySelector("ul").appendChild(li)
+    document.querySelector("form").reset()
+  }
+  else{
+    alert("No Input")
+  }
+}
+function delet(event){
+  event.target.parentNode.remove();
+}
